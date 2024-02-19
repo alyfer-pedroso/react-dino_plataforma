7f9f6e3ce686e276dfd93461226174f40eb583a1
alyfer-pedroso <alyferleandroaraujopedroso@gmail.com>
2024-02-19 17:24:13 -0300

    dsadsadas

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
+import dinoLogo from "../assets/dinoLogo.jpg";
+
 import "./Header.css";
 
-export const Header = () => {
-  return <div>Header</div>;
+export const Header = (props) => {
+  return (
+    <header className="header">
+      <div>
+        <img src={dinoLogo} alt="logo dino" />
+      </div>
+    </header>
+  );
 };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
+import "./Login.css";
+import { Header } from "../components/Header";
+
 export const Login = () => {
-  return <div>Login</div>;
+  return (
+    <>
+      <div className="mobile-div">
+        <Header />
+      </div>
+    </>
+  );
 };
